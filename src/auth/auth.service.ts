aa4380f5b44456c0cf22081dcd2c4a3fa92a0f03
Bryan Chan <cky.bryan@gmail.com>
2020-09-30 16:41:25 -0400

    added logging

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -7,6 +7,8 @@ import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+  private logger = new Logger('AuthService');
+
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
     private jwtServicer: JwtService,
@@ -27,6 +29,7 @@ export class AuthService {
     }
     const payload: JwtPayload = { username };
     const accessToken = this.jwtServicer.sign(payload);
+    this.logger.debug(`Generated JWT Token with payload ${payload}`);
     return { accessToken };
   }
 }
