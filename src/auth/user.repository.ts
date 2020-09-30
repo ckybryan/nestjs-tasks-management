7152ea2a8f362f95c111f0a251d24d108ee4976c
Bryan Chan <cky.bryan@gmail.com>
2020-09-30 13:40:15 -0400

    added JWT and linked task with users

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,43 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.salt = await bcrypt.genSalt();
+    user.username = username;
+    user.password = await this.hashPassword(password, user.salt);
+
+    try {
+      await user.save();
+    } catch (e) {
+      if (e.code === '23505') {
+        // duplicate user name
+        throw new ConflictException('Username already exists');
+      } else throw new InternalServerErrorException();
+    }
+  }
+
+  async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt);
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else return null;
+  }
+}
