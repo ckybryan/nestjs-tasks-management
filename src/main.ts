08cada2b3b5f3889e9e45ba935c7473175423343
Bryan Chan <cky.bryan@gmail.com>
2020-09-30 19:10:02 -0400

    added cors settings

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+  }
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
 
